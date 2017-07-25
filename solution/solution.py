ee498ca655275ae2fa019f8caba30c0556864efa
hanchengwu <hancheng.wu22@gmail.com>
2017-07-25 19:45:51 +0000

    Solution explaination complete

--- solution/solution.py
+++ solution/solution.py
@@ -1,10 +1,10 @@
 def solved_selection_sort(ls):
-    for i in range(len(ls)):
-        lowest = i
-        for j in range(i, len(ls)):
-            if ls[lowest] > ls[j]:
-                lowest = j
-        temp = ls[lowest]
-        del ls[lowest]
-        ls.insert(i, temp)
-    return ls
+    for i in range(len(ls)): #iterates through list of 0 to length of ls - 1
+        lowest = i #for now, assume number at index i is the lowest
+        for j in range(i, len(ls)): #iterates through list of i to length of ls - 1
+            if ls[lowest] > ls[j]: #if the number at index of the lowest is greater than number at index of i
+                lowest = j #actually the number at index of i is the lowest
+        temp = ls[lowest] #current lowest found, put that in a temporary variable
+        del ls[lowest] #delete the lowest
+        ls.insert(i, temp) #put it to index i (where we keep track of where to put it)
+    return ls #list is sorted by the time loop is done
