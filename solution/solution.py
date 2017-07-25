6eedc12466e02d3acd522de5cda208a146dcf24c
haw230 <hancheng.wu22@gmail.com>
2017-07-25 15:31:50 -0600

    Update solution.py

--- solution/solution.py
+++ solution/solution.py
@@ -4,7 +4,6 @@ def solved_selection_sort(ls):
         for j in range(i, len(ls)): #iterates through list of i to length of ls - 1
             if ls[lowest] > ls[j]: #if the number at index of the lowest is greater than number at index of i
                 lowest = j #actually the number at index of i is the lowest
-        temp = ls[lowest] #current lowest found, put that in a temporary variable
-        del ls[lowest] #delete the lowest
-        ls.insert(i, temp) #put it to index i (where we keep track of where to put it)
+        ls.insert(i, ls[lowest]) #insert the item at index i
+        del ls[lowest + 1] #delete the item at the old position (+1 because insert shifted all elements over by 1) so we don't have duplicates
     return ls #list is sorted by the time loop is done
