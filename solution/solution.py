03a11f4b8cbe4f08a6e4525855ccb73a2e69f3e1
haw230 <hancheng.wu22@gmail.com>
2017-07-26 15:19:34 -0600

    Update solution.py

--- solution/solution.py
+++ solution/solution.py
@@ -1,7 +1,7 @@
 def solved_selection_sort(ls):
     for i in range(len(ls)): #iterates through list of 0 to length of ls - 1
         lowest = i #for now, assume number at index i is the lowest
-        for j in range(i, len(ls)): #iterates through list of i to length of ls - 1
+        for j in range(i + 1, len(ls)): #iterates through list of i to length of ls - 1
             if ls[lowest] > ls[j]: #if the number at index of the lowest is greater than number at index of i
                 lowest = j #actually the number at index of i is the lowest
         ls.insert(i, ls[lowest]) #insert the item at index i
